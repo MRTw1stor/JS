1481300181effb357c184d44c28b7fab33f8f0b2
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-02-07 12:43:52 +0300

    Delete дз directory

--- дз/zadanie №2.js
+++
@@ -1,7 +0,0 @@
-// Дано двузначное число. Поменять местами цифры в числе.
-
-const number = 19
-let firstNumber = number % 10
-let secondNumber = Math.floor(number / 10)
-console.log("Дано число:", number)
-console.log("Число в обратном порядке:",(firstNumber * 10) + secondNumber);
--- дз/zadanie №3.js
+++
@@ -1,9 +0,0 @@
-// Вычислить дробную часть среднего арифметического двух заданных положительных вещественных чисел.
-
-const firstNumber = 28.28
-const secondNumber = 14.14
-let arifmetic = ((firstNumber + secondNumber) / 2)
-let roundNumber = Math.floor(arifmetic)
-let difference = arifmetic - roundNumber
-console.log("Даны два числа:",firstNumber,"и",secondNumber);
-console.log("Дробная часть среднего арифметического двух заданных чисел:",difference.toFixed(3));
