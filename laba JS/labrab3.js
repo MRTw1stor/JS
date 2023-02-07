3e901104b9a0b17c76585838c5051fdce5a843fe
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-02-07 12:36:36 +0300

    Delete laba JS directory

--- laba JS/labrab3.js
+++
@@ -1,8 +0,0 @@
-// Написать генератор нечётных случайных чисел в диапазоне между n и m
-// Учесть, что n и m могут быть отрицательными
-
-const min = 300;
-const max = -1;
-let rand = Math.floor(Math.random() * (max - min) + min);
-let chislo = rand + 1 - Math.abs(rand % 2);
-console.log(chislo);
