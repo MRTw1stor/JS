6fbe1742a351cccd38abd87346be3f1db0ac37e1
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-02-28 08:54:47 +0300

    Add files via upload

---
+++ Ср/sr №1/zadanie3.js
@@ -0,0 +1,10 @@
+// Вариант №1
+
+let number = 1;
+
+if (number == 0) {
+  console.log("Вы ввели число 0")} 
+else if (number == 1) {
+  console.log("Вы ввели число 1")} 
+else if (number == 2 || number == 3){
+  console.log("Вы ввели число 2, а может и 3")}
