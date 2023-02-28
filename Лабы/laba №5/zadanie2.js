69111e83291ca876f57c2cdb4586eac9e5a649fa
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-02-28 08:50:57 +0300

    Add files via upload

---
+++ Лабы/laba №5/zadanie2.js
@@ -0,0 +1,6 @@
+// Вариант №1
+
+let array = [5, -3, 7]
+
+for(let pop in array){
+    console.log("number ${i}!");}
---
+++ Лабы/laba №5/zadanie5.js
@@ -0,0 +1,20 @@
+// Вариант №1
+
+let numberDay = 1
+
+if (numberDay == 1) {
+  console.log("Понедельник")} 
+else if (numberDay == 2) {
+  console.log("Вторник")} 
+else if (numberDay == 3) {
+  console.log("Среда")} 
+else if (numberDay == 4) {
+  console.log("Четверг")} 
+else if (numberDay == 5) {
+  console.log("Пятница")} 
+else if (numberDay == 6) {
+  console.log("Суббота")} 
+else if (numberDay == 7) {
+  console.log("Воскресенье")}
+else{console.log("Вы ошиблись")}
+
