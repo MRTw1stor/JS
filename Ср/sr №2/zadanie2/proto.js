1fcd4af70b22a313363fd70d8065188c2acab118
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-03-23 11:32:56 +0300

    Update proto.js

--- Ср/sr №2/zadanie2/proto.js
+++ Ср/sr №2/zadanie2/proto.js
@@ -1,10 +1,28 @@
 alert("Привет")
 
-const number = 34
-let input = prompt("Введите число")
+const number = Math.floor(Math.random() * 10)
+let input = prompt("Программа загадала число, попробуйте его угадать")
 
 function game() {
-    if(input == NaN){
-        alert("Введите число")
+    if (input === null) {
+        return
+    } 
+    while(input !== null){
+        if (isNaN(parseInt(input))) {
+            input = prompt("Введите число!")
+            game()
+        } 
+        else if (parseInt(input) > number) {
+            input = prompt("Меньше!")
+        } 
+        else if (parseInt(input) < number) {
+            input = prompt("Больше!")
+        } 
+        else {
+            alert("Вы отгадали число!")
+            return
+        }
     }
+    game()
 }
+game()
