fe4d3662e691fc300aaaf6da58378353638d2f1f
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-05-10 22:09:43 +0300

    Add files via upload

--- Работы/student/proto.js
+++ Работы/student/proto.js
@@ -1,5 +1,3 @@
-import { addStudent } from "./prototip.js";
-
 let students = [
   {
     name: "Степан",
@@ -56,7 +54,81 @@ function Status(date) {
 
 otrisovka();
 
-document.querySelector(".test").addEventListener("click", () => {
-    document.querySelector(".modal").style.display = "block";
-    document.querySelector(".modalBack").style.display = "block";
+document.querySelector(".new").addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "block";
+  document.querySelector(".modalBack").style.display = "block";
+
+document.querySelector("#close").addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "";
+  document.querySelector(".modalBack").style.display = "";
+})
+
 });
+
+let table = document.getElementById("students");
+let th = table.getElementsByTagName("th");
+
+for (let i = 0; i < th.length; i++) {
+  th[i].addEventListener("click", function () {
+    let rows = table.getElementsByTagName("tr");
+    let rowArray = [];
+
+    for (let j = 1; j < rows.length; j++) {
+      rowArray[j - 1] = rows[j];
+    }
+    let index = i;
+    let isAscending = true;
+
+    if (th[i].classList.contains("sorted-asc")) {
+      isAscending = false;
+      th[i].classList.remove("sorted-asc");
+      th[i].classList.add("sorted-desc");
+    }
+    else if (th[i].classList.contains("sorted-desc")) {
+      isAscending = true;
+      th[i].classList.remove("sorted-desc");
+      th[i].classList.add("sorted-asc");
+    }
+    else {
+      isAscending = true;
+      for (let k = 0; k < th.length; k++) {
+        if (k != index) {
+          th[k].classList.remove("sorted-asc");
+          th[k].classList.remove("sorted-desc");
+        }
+      }
+      th[i].classList.add("sorted-asc");
+    }
+    rowArray.sort(function (a, b) {
+      let aVal = a.getElementsByTagName("td")[index].textContent;
+      let bVal = b.getElementsByTagName("td")[index].textContent;
+      if (isAscending) {
+        return aVal.localeCompare(bVal);
+      } else {
+        return bVal.localeCompare(aVal);
+      }
+    });
+    for (let m = 0; m < rowArray.length; m++) {
+      table.appendChild(rowArray[m]);
+    }
+  })
+}
+
+// function addStudent() {
+//     let name = document.querySelector("#name");
+//     let surname = document.querySelector("#surname");
+//     let secondname = document.querySelector("#secondname");
+//     let birthdate = document.querySelector("#birdh");
+//     let teachstart = document.querySelector("#teachstart");
+//     let faculty = document.querySelector("#faculty");
+//     let studentList = {
+//       name: name.value,
+//       surname: surname.value,
+//       secondname: secondname.value,
+//       birthdate: birthdate.value,
+//       learnstart: teachstart.value,
+//       faculty: faculty.value,
+//     };
+//     students.push(studentList);
+// }
+// document.querySelector("#add").addEventListener("click",addStudent)
