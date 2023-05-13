03161ffd1542c5b747b08c0d93f88fc458fc4213
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-05-13 18:31:45 +0300

    Update zadanie6.js

--- Ср/sr №4/zadanie6.js
+++ Ср/sr №4/zadanie6.js
@@ -1,7 +1,7 @@
 let string = "var_test_text"
 let stringg = string.split("_")
 for(let i = 1; i < stringg.length; i++){
-    stringg[i] = stringg[i].slice(0, 1).toUpperCase() + stringg[i].slice(1)
+    stringg[i] = stringg[i].slice(0, 1).toUpperCase() + stringg[i].slice(1).toLowerCase()
 }
 let str = stringg.join("")
 console.log(str)
