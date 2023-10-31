00a927e1f4e793c0897e472abede05e7fcd8de93
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-10-31 08:51:07 +0300

    Add files via upload

---
+++ 1234/src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import "./Mainpage.modules.css"
+import Header from "./header/Header";
+import Body from "./body/Body";
+
+const MainPage = () => {
+    return (
+      <div className="MainPage">
+        <Header/>
+        <Body/>
+      </div>
+    )
+}
+
+export default MainPage
