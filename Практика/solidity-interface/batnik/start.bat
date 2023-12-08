d62cd4a96e585418c4976982a169e98f73e5931b
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-12-08 09:38:48 +0300

    Delete Практика/solidity-interface directory

--- Практика/solidity-interface/batnik/start.bat
+++
@@ -1,3 +0,0 @@
-start geth --datadir "C:\Users\Blockchain\Desktop\solidity-interface\batnik" --http --http.port 8545 --http.api "web3,eth,net,personal,miner" --http.corsdomain "*" --allow-insecure-unlock --networkid 7133
-timeout /t 10
-start cmd /k geth attach http://localhost:8545
