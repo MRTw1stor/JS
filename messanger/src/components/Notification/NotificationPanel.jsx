ba024c8ed1bbb2d975a5cd5783cc7f0c0d2cc6b3
MRTw1stor <123019975+MRTw1stor@users.noreply.github.com>
2023-12-11 21:50:18 +0300

    Add files via upload

---
+++ messanger/src/components/Notification/NotificationPanel.jsx
@@ -0,0 +1,11 @@
+import "./NotificationPanel.modules.css"
+
+const NotificationPanel = () => {
+    return(
+        <div id="notificationPanel">
+            <span id="Notification">Notification</span>
+        </div>
+    )
+}
+
+export default NotificationPanel
---
+++ messanger/src/components/Settings/SettingsPanel.jsx
@@ -0,0 +1,11 @@
+import "./SettingsPanel.modules.css"
+
+const SettingsPanel = () => {
+    return(
+        <div id="settingsPanel">
+            <span id="Settings">Settings</span>
+        </div>
+    )
+}
+
+export default SettingsPanel
