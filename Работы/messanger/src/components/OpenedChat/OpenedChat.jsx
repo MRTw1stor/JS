c73b25b0397887702d2d720fede1514a57a61f17
Otakawa <123019975+MRTw1stor@users.noreply.github.com>
2024-03-26 08:30:22 +0300

    Add files via upload

---
+++ Работы/messanger/src/components/OpenedChat/OpenedChat.jsx
@@ -0,0 +1,11 @@
+const OpenedChat = () => {
+    return (
+        <div>
+            <div>
+                nekit lox ebaniy))))))))
+            </div>
+        </div>
+    )
+};
+
+export default OpenedChat;
